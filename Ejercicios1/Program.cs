fc04b83128696fe511fc3fa117da9d593bf5f51f
beca2 <beca2@I4Y-DV4>
2017-01-11 15:11:20 +0100

    Calcular area y longitud de la circunferencia

--- Ejercicios1/Program.cs
+++ Ejercicios1/Program.cs
@@ -10,6 +10,25 @@ namespace Ejercicios1
     {
         static void Main(string[] args)
         {
+            //Declaramos variables
+            double longitudRadio;
+            double areaCirculo;
+            double longitudCircunferencia;
+            const double pi1 = 3.1416;
+            const double pi2 = 3.1415927;
+
+            //Introducimos valor de la longitud del radio
+            Console.WriteLine("Introduzca longitud de radio:");
+            string radio = Console.ReadLine();
+            double.TryParse(radio, out longitudRadio);
+
+            //Realizamos las operaciones del área del circulo y la longitud de la circunferencia correspondientes
+            areaCirculo = pi2 * Math.Pow(longitudRadio, 2);
+            longitudCircunferencia = 2 * pi2 * longitudRadio;
+
+            Console.WriteLine(""+ areaCirculo +"  "+ longitudCircunferencia);
+
+            Console.ReadLine();
         }
     }
 }
